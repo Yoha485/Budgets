dc3570e57ba4917df5c10db8964c9f8169d96106
ivan_ignatenko281 <i.ignatenko@applegalhelp.ru>
2022-04-03 13:52:13 +0300

    fix db conn

--- backend/src/database-connection.service.ts
+++ backend/src/database-connection.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv'
 
+dotenv.config()
 @Injectable()
 export class DatabaseConnectionService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
-      name: 'default',
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'user',
-      password: 'pass',
-      database: 'budgets-db',
-      synchronize: true,
-      dropSchema: false,
-      logging: false,
-      entities: ['dist/**/*.entity.js'],
-    };
+			name: 'default',
+			type: 'postgres',
+			host: process.env.DATABASE_HOST,
+			port: +process.env.DATABASE_PORT,
+			username: process.env.DATABASE_USER,
+			password: process.env.DATABASE_PASS,
+			database: process.env.DATABASE_NAME,
+			synchronize: true,
+			dropSchema: false,
+			logging: false,
+			entities: ['dist/**/*.entity.js'],
+		};
   }
 }
