90ad06d1be03e13bd3e7b050eeeb4b434d150cf4
Anton Morozov <antonmorozoff64@gmail.com>
2022-04-05 14:18:28 +0600

    remove dist

--- backend/dist/auth/auth/auth.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { LoginDto, RegisterDto } from 'src/user/user.dto';
-import { AuthService } from './auth.service';
-export declare class AuthController {
-    private authService;
-    constructor(authService: AuthService);
-    register(credentials: RegisterDto): Promise<any>;
-    login(credentials: LoginDto): Promise<any>;
-}
--- backend/dist/user/user.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractEntity } from 'src/common/abstract-entity';
-export declare class UserEntity extends AbstractEntity {
-    email: string;
-    username: string;
-    password: string;
-    comparePassword(attempt: string): boolean;
-    toJson(): Record<string, any>;
-}
