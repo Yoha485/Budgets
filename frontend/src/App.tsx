85af36f657b1d95882ab15f8bd1baab80173511f
Anton Morozov <antonmorozoff64@gmail.com>
2022-04-17 15:38:03 +0600

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="font-bold">
+      App
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "648px",
+      md: "748px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  plugins: [],
+};
