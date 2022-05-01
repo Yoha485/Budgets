490a53c5990564b9158463622b128fd85ea61729
Anton Morozov <antonmorozoff64@gmail.com>
2022-05-01 18:26:31 +0600

    Change api url

--- frontend/src/app/api/index.ts
+++ frontend/src/app/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:4000/' });
+const API = axios.create({ baseURL: 'https://nestjs-budgets.herokuapp.com/' });
 
 API.interceptors.request.use((req: any) => {
 	if (localStorage.getItem('user')) {
